ab4923eddc32461ae99640c9ce801817add5bafa
Rolol335 <nimaeva250371@gmail.com>
2026-05-16 10:59:24 +0800

    ifk

--- Ping-Pong.algo/main.py
+++ Ping-Pong.algo/main.py
@@ -8,9 +8,7 @@ window = display.set_mode((window_width, window_height))
 #классы
 class GameSprite(sprite.Sprite):
  #конструктор класса
-   def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
-       #вызываем конструктор класса (Sprite):
-       sprite.Sprite.__init__(self)
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
 
 
        #каждый спрайт должен хранить свойство image - изображение
@@ -22,12 +20,42 @@ class GameSprite(sprite.Sprite):
        self.rect = self.image.get_rect()
        self.rect.x = player_x
        self.rect.y = player_y
-class Player(GameSprite):
-    def update(self):
-        keys = key.get_pressed()
-
-
-
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+class Player_left(GameSprite):
+    def __init__(self, image, player_x, player_y, size_x, size_y, player_speed):
+        def update(self):
+            keys = key.get_pressed()
+            if keys[K_W] and self.rect.y > 70:
+                self.rect.y -= self.speed
+            if keys[K_S] and self.rect.y < window_height - 70:
+                self.rect.y += self.speed
+player1 = Player_left('wall.png', 350, 50, 70, 70, 3)
+class Player_right(GameSprite):
+    def __init__(self, image, player_x, player_y, size_x, size_y, player_speed):
+        def update(self):
+            keys = key.get_pressed()
+            if keys[K_UP] and self.rect.y > 70:
+                self.rect.y -= self.speed
+            if keys[K_DOWN] and self.rect.y < window_height - 70:
+                self.rect.y += self.speed
+player2 = Player_right('wall.png', 350, 450, 70, 70, 3)
 class Ball(GameSprite):
-    
+    def __init__(self, image, player_x, player_y, size_x, size_y, player_speed):
+        def update(self):
+            if self.rect.y >=50:
+                self.speed = self.speed * -1
+            if self.rect.y <= window_height - 50:
+                self.speed = self.speed * -1
+
+ball = Ball('tennis_ball.png', 350, 250, 50, 50, 4)
+ball2 = Ball('tennis_ball.png', 350, 250, 50, 50, 5)            
 #игровой цикл
+game = True
+while game:
+    player1.reset()
+    player2.reset()
+    ball.reset()
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
